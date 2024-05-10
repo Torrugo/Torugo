ee241c8766ec9a99bedd63a8fd987c2c73391b82
Torrugo <Prospiderpro@gmail.com>
2024-05-10 09:22:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function Somar(a,b){
+alert(a+b)
+
+}
+function Subtrair(a,b){
+    alert(a-b)
+}
+function Dividir(a,b){
+    alert(a/b)
+}
+function Multiplicação(a,b){
+    alert(a*b)
+}
+
+Multiplicação(10,20)
